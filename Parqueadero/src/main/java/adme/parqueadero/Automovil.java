d63059ba8c02360da2eba2fc6e6dfbe79226fc95
Andrés Daniel Medina Erazo <142424780+DANIELMEDINA0408@users.noreply.github.com>
2023-11-17 11:30:29 -0500

    Parcial II

---
+++ Parqueadero/src/main/java/adme/parqueadero/Automovil.java
@@ -0,0 +1,27 @@
+
+package adme.parqueadero;
+/**
+ *
+ * @author Andrés Daniel Medina Erazo (104623010363)
+ */
+public class Automovil extends Vehiculo{
+      private int numeroPuertas;
+    // Constructores, getters y setters
+
+    // Otros métodos específicos para automóviles
+    
+    public Automovil(int numeroPuertas, String marca, String modelo, String placa, int horaEntrada) {
+        super(marca, modelo, placa, horaEntrada);
+        this.numeroPuertas = numeroPuertas;
+    }
+    
+    public int getNumeroPuertas() {
+        return numeroPuertas;
+    }
+
+    public void setNumeroPuertas(int numeroPuertas) {
+        this.numeroPuertas = numeroPuertas;
+    }
+    
+  
+}
---
+++ Parqueadero/src/main/java/adme/parqueadero/Motocicleta.java
@@ -0,0 +1,26 @@
+package adme.parqueadero;
+/**
+ *
+ * @author Andrés Daniel Medina Erazo (104623010363)
+ */
+public class Motocicleta extends Vehiculo{
+      private int cilindrada;
+
+    // Constructores, getters y setters
+    // Otros métodos específicos para motocicletas
+
+    public Motocicleta(int cilindrada, String marca, String modelo, String placa, int horaDeEntrada) {
+        super(marca, modelo, placa, horaDeEntrada);
+        this.cilindrada = cilindrada;
+    }
+
+    public int getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(int cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+    
+    
+}
